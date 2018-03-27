64379f9640ca34afae7ecdec96698c3d6c25a253
Michael Heimes <30752366+mihe90@users.noreply.github.com>
2018-03-27 14:10:41 +0200

    Upload new version

--- build/scripts-3.6/platereader.py
+++ build/scripts-3.6/platereader.py
@@ -82,6 +82,10 @@ if a == 'Unnamed: 0':
     df= pd.merge(df, plate_row, on = 'row').drop(['row'], axis = 1).reset_index(drop=True)
     df = df.melt(id_vars=['plate_row'], var_name = 'plate_column').dropna()
 
+#replace #sat and convert to numeric.
+#df['value']= df['value'].replace('#Sat', float('inf'))
+df = df[df.value != '#Sat']
+df['value']= df['value'].astype('str').str.replace(',','.').astype('float')
 
 #back to wide-table format
 
@@ -108,8 +112,14 @@ while True:
         df['plate_column'] = df['plate_column'].astype('int')
         well = ['plate_row','plate_column']
         df = pd.merge(df, plate, on = well)
-        df = df.pivot_table(index=[a], columns = ['sample'], values= 'value').reset_index().rename_axis(None, 1)
-        break
+        if a == 'Unnamed: 0':
+            a= 'time (s)'
+            df[a] = 'Endpoint'
+            df = df.pivot_table(index=[a], columns = ['sample'], values= 'value').reset_index().rename_axis(None, 1)
+            break
+        else:
+            df = df.pivot_table(index=[a], columns = ['sample'], values= 'value').reset_index().rename_axis(None, 1)
+            break
 
 ###sort columns
 def natural_keys(text):
